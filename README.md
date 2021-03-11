91f77e68ac0246ada17cd24cadd34c7c3b9e12b2
prosayj@gmail.com <15665662468@163.com>
2021-03-12 02:01:27 +0800

    init
    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# think-in-jdk1.8-241
- 
+# thinking-in-jdk8-241
+- https://blog.csdn.net/m0_37258694/article/details/113870843
+- src.zip解压缩
+- 引入
+- https://gitee.com/xushiyu/jdk18research/tree/master/src/java
---
+++ src/test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world");
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+
+    }
+}
